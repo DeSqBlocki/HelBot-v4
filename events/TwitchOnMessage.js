d1660fb83e1b74621c34de24c1bad94cd337675d
DeSqBlocki <theblockcraftlp@t-online.de>
2023-09-28 09:11:02 +0200

    fixed userstate requirements

--- events/TwitchOnMessage.js
+++ events/TwitchOnMessage.js
@@ -21,6 +21,19 @@ async function getIDByName(user) {
     const result = await Helix.users.get(user);
     return result.data[0].id
 }
+async function verifyUser(channel, username) {
+    let db = mClient.db("shoutouts")
+    let col = db.collection(channel)
+
+    let query = { user: username }
+    let status = await col.findOne(query)
+    if (!status) {
+        DiscordClient.emit('TwitchShoutout', "VIP", channel, username)
+        await col.insertOne({
+            user: username
+        })
+    }
+}
 module.exports = {
     name: 'TwitchOnMessage',
     once: false,
@@ -36,42 +49,28 @@ module.exports = {
             TwitchClient.say(channel, 'KAW!')
         }
 
+        
         if (message.startsWith(TTV_Prefix)) {
             const args = message.substring(1).split(" ")
             const cmd = args[0]
             switch (cmd) {
-                case "test":
-                    //TwitchClient.say(channel, "/me This is a test message")
-                    break;
+                // case "test":
+                //     TwitchClient.say(channel, "/me This is a test message")
+                //     break;
                 case "mode":
-                    if (userstate.username != "desq_blocki") { return }
+                    if (userstate.badges.moderator != 1 && userstate.badges.broadcaster != 1) { break } // return stops all further operations, so you should just break out of switch
                     let regex = /^(on|off)$/
-                    if (!args[1].match(regex)) { return }
+                    if (!args[1].match(regex)) { break }
                     updateChatMode(await getIDByName(channel.substring(1)), args[1])
                     break;
                 default:
                     break;
             }
         }
-
-        // ---------------VIP Handler--------------------
+        // ---------------VIP Handler-------------------
         if (!userstate.badges?.vip) { return }
         verifyUser(channel, userstate.username)
         // ---------------------------------------------
-
-        async function verifyUser(channel, username) {
-            let db = mClient.db("shoutouts")
-            let col = db.collection(channel)
-
-            let query = { user: username }
-            let status = await col.findOne(query)
-            if (!status) {
-                DiscordClient.emit('TwitchShoutout', "VIP", channel, username)
-                await col.insertOne({
-                    user: username
-                })
-            }
-        }
     }
 
 }
